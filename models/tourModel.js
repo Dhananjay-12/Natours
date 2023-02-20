e5e062a913d569a6d7f05b5c5638fe468854ebe0
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-02-20 22:53:53 +0530

    tour and user schema

---
+++ models/tourModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: [true, 'A tour must have a name'],
+  },
+  rating: {
+    type: Number,
+
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  difficulty: String,
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
