d58655a147ef4ff4179bc142a4a2b3c8c1c0e1a7
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-02-22 21:09:16 +0530

    refactoring added by api features class

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -31,42 +31,61 @@ const getAliasTour = (req, res, next) => {
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
   next();
 };
-const getAllTours = async (req, res) => {
-  try {
-    const queryObj = { ...req.query };
+
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
-    //ADVANCED FILTERING
     let queryStr = JSON.stringify(queryObj);
-
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
 
-    let query = Tour.find(JSON.parse(queryStr));
-    //SORT THE ITEMS
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    }
-    //SHOWING SPECIFIC DATA ONLY
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
     } else {
-      query = query.select('-__v');
+      this.query = this.query.sort('-createdAt');
     }
-    //PAGINATION
-    const page = Number(req.query.page) || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-    ///case when skip value exceeds total no documents
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
+    return this;
+  }
 
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
+
+const getAllTours = async (req, res) => {
+  try {
     //EXECUTE QUERY
-    const tour = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tour = await features.query;
     //SEND RESPONSE
     res.status(200).json({
       message: 'SUCCESS',
@@ -158,3 +177,35 @@ module.exports = {
   getAliasTour,
   checkBody,
 };
+//OLD STUFF FOR GET ALL TOURS WITH FILTERING
+// const queryObj = { ...req.query };
+// const excludedFields = ['page', 'sort', 'limit', 'fields'];
+// excludedFields.forEach((el) => delete queryObj[el]);
+// //ADVANCED FILTERING
+// let queryStr = JSON.stringify(queryObj);
+
+// queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+// let query = Tour.find(JSON.parse(queryStr));
+//SORT THE ITEMS
+// if (req.query.sort) {
+//   const sortBy = req.query.sort.split(',').join(' ');
+//   query = query.sort(sortBy);
+// }
+//SHOWING SPECIFIC DATA ONLY
+// if (req.query.fields) {
+//   const fields = req.query.fields.split(',').join(' ');
+//   query = query.select(fields);
+// } else {
+//   query = query.select('-__v');
+// }
+//PAGINATION
+// const page = Number(req.query.page) || 1;
+// const limit = req.query.limit * 1 || 100;
+// const skip = (page - 1) * limit;
+// query = query.skip(skip).limit(limit);
+///case when skip value exceeds total no documents
+// if (req.query.page) {
+//   const numTours = await Tour.countDocuments();
+//   if (skip >= numTours) throw new Error('This page does not exist');
+// }
