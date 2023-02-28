d7951a5ec5133ee3fc47c817e9f8a8bdb4fc52ed
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-03-01 00:42:02 +0530

    get all user in DB route added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,17 @@
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'Error',
-    message: 'This route is not defined yet..',
+const User = require('./../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+const getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      users,
+    },
   });
-};
+});
+
 const getUser = (req, res) => {
   res.status(500).json({
     status: 'Error',
