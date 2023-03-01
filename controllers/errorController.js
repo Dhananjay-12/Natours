94ed9e54b65f5fc5c8553f6fc8167307ed8a9c67
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-03-02 03:21:21 +0530

    token expired error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -17,6 +17,9 @@ const handleValidationErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleJWTExpiredError = (err) =>
+  new AppError('Your token has expired! Please log in again', 401);
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -55,6 +58,8 @@ module.exports = (err, req, res, next) => {
     if (error['_message'] === 'Validation failed') {
       error = handleValidationErrorDB(error);
     }
+    if (error.name === 'TokenExpiredError')
+      error = handleJWTExpiredError(error);
     sendErrorProd(error, res);
   }
 };
