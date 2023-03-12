327d405369a5156ef44f1c1be80a056abcab1eb4
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-03-13 01:38:24 +0530

    filterObj to allow specific fields

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,23 @@
 const User = require('./../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  // const newObj = {};
+  // Object.keys(obj).forEach((el) => {
+  //   if (allowedField.includes(el)) {
+  //     newObj[el] = obj[el];
+  //   }
+  // });
+  // return newObj;
+  const newObj = {};
+  for (let key in obj) {
+    if (allowedFields.includes(key)) {
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
+};
 
 const getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -9,6 +27,24 @@ const getAllUsers = catchAsync(async (req, res, next) => {
     data: {
       users,
     },
+  });
+});
+
+const updateMe = catchAsync(async (req, res, next) => {
+  //if user send password
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('This route does not update password', 400));
+  }
+
+  const filterBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findOneAndUpdate(req.user.id, filterBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'SUCCESS',
+    user: updatedUser,
   });
 });
 
@@ -37,4 +73,11 @@ const deleteUser = (req, res) => {
   });
 };
 
-module.exports = { getAllUsers, getUser, createUser, updateUser, deleteUser };
+module.exports = {
+  getAllUsers,
+  updateMe,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+};
