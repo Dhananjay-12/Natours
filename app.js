a8885da4b80021606db79a3c834ed78110a59d8f
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-03-13 20:54:43 +0530

    adding middleware: expressrate limit & helmet

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'developement') app.use(morgan('dev'));
 
-app.use(express.json());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests ! Try again later in an hour',
+});
+
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '10kb' }));
 app.use(express.static(`${__dirname}/public`));
 app.use(cookieParser());
 app.use('/api/v1/tours', tourRouter);
