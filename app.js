cb6b3e871245ef2920d250642ec512e18295b328
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-03-13 21:09:41 +0530

    data sanitisation implemented

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -22,6 +24,10 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 app.use(express.json({ limit: '10kb' }));
+
+app.use(mongoSanitize());
+app.use(xss());
+
 app.use(express.static(`${__dirname}/public`));
 app.use(cookieParser());
 app.use('/api/v1/tours', tourRouter);
