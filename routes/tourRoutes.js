4f72061c87ab3e5f09f085ead7e0c4a8ca3a01c5
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-03-20 23:50:29 +0530

    refactoring using getParams

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('./../controllers/authController');
-const reviewController = require('./../controllers/reviewController');
+// const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./../routes/reviewRoutes');
 const {
   getAllTours,
   getTour,
@@ -14,6 +15,15 @@ const {
   getMonthlyPlan,
 } = require('./../controllers/tourController');
 
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createReview
+//   );
+
+router.use('/:tourId/reviews', reviewRouter);
 router.route('/top-5-cheap').get(getAliasTour, getAllTours);
 router.route('/tour-stats').get(getTourStats);
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
@@ -33,11 +43,4 @@ router
     deleteTour
   );
 
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
-  );
 module.exports = router;
