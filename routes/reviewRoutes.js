7e9764789ee5c7adbf346dd843838f3fd2f0dedd
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-03-20 21:17:48 +0530

    Routes For Reviews

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('./../controllers/authController');
+const reviewController = require('./../controllers/reviewController');
+
+router
+  .get('/', reviewController.getAllReviews)
+  .post(
+    '/',
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
+  );
+
+module.exports = router;
