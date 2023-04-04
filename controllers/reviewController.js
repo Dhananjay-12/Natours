459b3a3335610ea78a2d8326af919d8e4c3bad7d
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-04-04 13:21:05 -0500

    set middleware and factroy function on review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -4,19 +4,25 @@ const APIFeatures = require('../utils/apiFeatures');
 const factory = require('./handlerFactory');
 const { request } = require('express');
 
-exports.createReview = catchAsync(async (req, res, next) => {
-  // nested routes
+exports.setTourUserIds = (req, res, next) => {
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
 
-  const newReview = await Review.create(req.body);
-  res.status(200).json({
-    status: 'SUCCESS',
-    data: {
-      review: newReview,
-    },
-  });
-});
+exports.createReview = factory.createOne(Review);
+
+// exports.createReview = catchAsync(async (req, res, next) => {
+//   // nested routes
+
+//   const newReview = await Review.create(req.body);
+//   res.status(200).json({
+//     status: 'SUCCESS',
+//     data: {
+//       review: newReview,
+//     },
+//   });
+// });
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
   let filter = {};
