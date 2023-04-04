ef3716fe4cd6436128fc0e85b6467bea656d3d99
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-04-04 14:21:18 -0500

    getall function added to reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,4 @@
 const Review = require('./../models/reviewModel');
-const catchAsync = require('./../utils/catchAsync');
-const APIFeatures = require('../utils/apiFeatures');
 const factory = require('./handlerFactory');
 const { request } = require('express');
 
@@ -11,32 +9,7 @@ exports.setTourUserIds = (req, res, next) => {
 };
 
 exports.createReview = factory.createOne(Review);
-
-// exports.createReview = catchAsync(async (req, res, next) => {
-//   // nested routes
-
-//   const newReview = await Review.create(req.body);
-//   res.status(200).json({
-//     status: 'SUCCESS',
-//     data: {
-//       review: newReview,
-//     },
-//   });
-// });
-
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
-  const review = await Review.find(filter);
-  //SEND RESPONSE
-  res.status(200).json({
-    message: 'SUCCESS',
-    results: review.length,
-    data: {
-      review,
-    },
-  });
-});
-
+exports.getReview = factory.getOne(Review);
+exports.getAllReviews = factory.getAll(Review);
 exports.deleteReview = factory.deleteOne(Review);
 exports.updateReview = factory.updateOne(Review);
