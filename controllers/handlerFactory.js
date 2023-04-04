f8bd0f4ee4e7f437dc9214f6068d7edc423a34e8
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-04-04 14:20:50 -0500

    get all model function

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,5 +1,6 @@
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
@@ -37,7 +38,45 @@ exports.createOne = (Model) =>
     res.status(201).json({
       message: 'SUCCESS',
       data: {
-        tour: doc,
+        data: doc,
+      },
+    });
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    //trick to allow nested GET on reviews
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+    //EXECUTE QUERY
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const doc = await features.query;
+    //SEND RESPONSE
+    res.status(200).json({
+      message: 'SUCCESS',
+      data: {
+        data: doc,
       },
     });
   });
