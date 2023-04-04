f8dd2ffd683854e5ccc89b935d3eb13ad644c2a8
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-04-04 14:21:43 -0500

    getAll function added to tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const Tour = require('./../models/tourModel');
-const APIFeatures = require('../utils/apiFeatures');
+
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
@@ -22,77 +22,11 @@ const getAliasTour = (req, res, next) => {
   next();
 };
 
-const getAllTours = catchAsync(async (req, res, next) => {
-  //EXECUTE QUERY
-  const features = new APIFeatures(Tour.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
-  const tour = await features.query;
-  //SEND RESPONSE
-  res.status(200).json({
-    message: 'SUCCESS',
-    data: {
-      tour,
-    },
-  });
-});
-//PARTICULAR TOUR
-
-const getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id).populate('reviews');
-
-  if (!tour) {
-    return next(new AppError('No tour found with that ID', 404));
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-});
-
-//CREATE A NEW TOUR
-
+const getAllTours = factory.getAll(Tour);
+const getTour = factory.getOne(Tour, { path: 'reviews' });
 const createTour = factory.createOne(Tour);
-// const createTour = catchAsync(async (req, res, next) => {
-//   const newTour = await Tour.create(req.body);
-//   res.status(201).json({
-//     message: 'SUCCESS',
-//     data: {
-//       tour: newTour,
-//     },
-//   });
-// });
-//UPDATE TOUR
 const updateTour = factory.updateOne(Tour);
-
-// const updateTour = catchAsync(async (req, res, next) => {
-//   const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-//     new: true,
-//     runValidators: true,
-//   });
-//   if (!tour) {
-//     return next(new AppError(`No data exist for id: ${req.params.id}`, 404));
-//   }
-//   res.status(200).json({ message: 'Data Updated Successfully', tour });
-// });
-
-//DELETE TOUR
-
 const deleteTour = factory.deleteOne(Tour);
-
-// const deleteTour = catchAsync(async (req, res, next) => {    //////OLD WAY
-//   const tour = await Tour.findByIdAndDelete(req.params.id);
-
-//   res.status(204).json({
-//     message: 'Deleted Successfully',
-//     data: 'NULL',
-//   });
-// });
 
 const getTourStats = catchAsync(async (req, res, next) => {
   const stats = await Tour.aggregate([
@@ -179,49 +113,3 @@ module.exports = {
   getTourStats,
   getMonthlyPlan,
 };
-//OLD STUFF FOR GET ALL TOURS WITH FILTERING
-
-// const queryObj = { ...req.query };
-// const excludedFields = ['page', 'sort', 'limit', 'fields'];
-// excludedFields.forEach((el) => delete queryObj[el]);
-// //ADVANCED FILTERING
-// let queryStr = JSON.stringify(queryObj);
-
-// queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-// let query = Tour.find(JSON.parse(queryStr));
-//SORT THE ITEMS
-// if (req.query.sort) {
-//   const sortBy = req.query.sort.split(',').join(' ');
-//   query = query.sort(sortBy);
-// }
-//SHOWING SPECIFIC DATA ONLY
-// if (req.query.fields) {
-//   const fields = req.query.fields.split(',').join(' ');
-//   query = query.select(fields);
-// } else {
-//   query = query.select('-__v');
-// }
-//PAGINATION
-// const page = Number(req.query.page) || 1;
-// const limit = req.query.limit * 1 || 100;
-// const skip = (page - 1) * limit;
-// query = query.skip(skip).limit(limit);
-///case when skip value exceeds total no documents
-// if (req.query.page) {
-//   const numTours = await Tour.countDocuments();
-//   if (skip >= numTours) throw new Error('This page does not exist');
-// }
-
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-// );
-
-// const checkId = (req, res, next, val) => {
-//   if (val > tours.length)
-//     return res.status(404).json({
-//       status: 'Fail',
-//       message: 'Invalid ID',
-//     });
-//   next();
-// };
